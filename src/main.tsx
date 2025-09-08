0e5041a5859c650d072d7f07c31d96544e8a7154
ashishrahi <148324855+ashishrahi@users.noreply.github.com>
2025-09-09 00:29:39 +0530

    updated code

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "keen-slider/keen-slider.min.css";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 createRoot(document.getElementById("root")!).render(<App />);
